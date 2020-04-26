42432e58a08e7d29da989dd8f8ed80aac8b8c703
Ubuntu <ubuntu@ip-172-29-80-201.ec2.internal>
2020-04-26 21:32:05 +0000

    Added chef-repo

---
+++ setup/chef-repo/gdc-workflow-runner/metadata.rb
@@ -0,0 +1,22 @@
+  
+name 'gdc-workflow-runner'
+maintainer 'NCI-GDC'
+maintainer_email 'support@nci-gdc.datacommons.io'
+license 'Apache-2.0'
+description 'Installs/Configures gdc-workflow-runner'
+version '0.1.0'
+chef_version '>= 14.0'
+
+source_url 'https://github.com/NCI-GDC/ec2-slurm-worker'
+issues_url 'https://github.com/NCI-GDC/ec2-slurm-worker/issues'
+
+supports 'ubuntu', '>= 18.04'
+supports 'amazon', '= 2'
+
+depends 'apt', '~> 7.2.0'
+depends 'yum-epel'
+depends 'os-hardening'
+
+depends 'ark', '~> 5.0.0'
+depends 'logrotate', '~> 2.2.2'
+depends 'hostsfile', '~> 3.0.1'
